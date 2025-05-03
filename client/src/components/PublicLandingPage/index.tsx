137af211cf6364c8bf71c61f54bc8da633668721
joaocorreiaprof <joaocorreiaprof@gmail.com>
2025-05-03 12:46:28 +0100

    Add Merriweather font files and CSS styles
    - Added OFL.txt for Merriweather font licensing information.
    - Created fonts.css to define the @font-face for Merriweather with variable font support.

--- client/src/components/PublicLandingPage/index.tsx
+++ client/src/components/PublicLandingPage/index.tsx
@@ -12,15 +12,21 @@ const PublicLandingPage = () => {
           <h2 className="header-title">Verdict</h2>
         </div>
         <div className="header-right">
-          <button>Sign In</button>
+          <button className="header-sign-in-btn">Sign In</button>
         </div>
       </header>
-      <img
-        src={frontImage}
-        alt="Person library"
-        className="person-library-image"
-      />
-
+      <section>
+        <img
+          src={frontImage}
+          alt="Person library"
+          className="person-library-image"
+        />
+        <div>
+          <p>Track what you’ve seen. Plan what’s next.</p>
+          <p>All your favorite movies, series, games and books in one place.</p>
+          <p>Enter your email to get started.</p>
+        </div>
+      </section>
       <section>{/*Section for a carrousel with top movies*/}</section>
       <section>{/*Section for a carrousel with top series*/}</section>
       <section>{/*Section for a carrousel with top games*/}</section>
