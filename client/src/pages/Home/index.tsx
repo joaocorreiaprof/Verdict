3fdcb43b1bc6a7100cb35a01eb1195454e569729
João Correia <joaocorreiaprof@gmail.com>
2025-06-10 18:39:33 +0100

    feat: implement logout functionality in Home component

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
   return (
     <div id="home">
       <p>Congrats</p>
       <p>You are authenticated</p>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
